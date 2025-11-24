d74a8c1b423995d9d5a806e1ac24823d107a682e
sarthak kumar <sarthakkumar10151@gmail.com>
2025-11-24 23:32:11 +0530

    Distinct numbers: sort and count

---
+++ SortingAndSearching/01_DistinctNumbers.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T> inline bool cmin(T &a, const T &b) { return b < a ? a = b, true : false; }
+template <typename T> inline bool cmax(T &a, const T &b) { return b > a ? a = b, true : false; }
+
+#ifdef SARTHAK_LOCAL
+#include "/Users/sarthakkumar/work/Cpp/Templates/Common/debug.hpp"
+#else
+#define debug(...) ((void)0)
+#endif
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr), cout << setprecision(12) << fixed;
+
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++) cin >> a[i];
+    sort(a.begin(), a.end());
+
+    int cnt = 0;
+    for(int i = 0; i < n; i++) {
+        if(i == 0 || a[i] != a[i - 1]) cnt++;
+    }
+    cout << cnt << '\n';
+
+    return 0;
+}
